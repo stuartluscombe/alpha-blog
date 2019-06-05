a60ca4652aa0250fe54f9e9f2294e5a5b7506f26
Stuart Luscombe <sluscombe@saintsfc.co.uk>
2019-06-05 14:55:51 +0100

    Add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -35,4 +40,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
+    
 end
